89e997e04dbe918bcdffbc126fc2fbfae155caf5
hamza <hamzaozgezici294@gmail.com>
2023-06-27 10:57:40 +0600

    added new model

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Laptops
+admin.site.register(Laptops)
 # Register your models here.
--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+class Items(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.CharField(max_length=20)
+    seller = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    buyer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
